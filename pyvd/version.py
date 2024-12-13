b0adda929d80438f823e9bef0226a257e7e9c24f
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2024-12-13 06:58:53 +0000

    use double quotes

--- pyvd/version.py
+++ pyvd/version.py
@@ -2,8 +2,8 @@
 Version and license information.
 """
 
-__all__ = ['__version__', '__versiondate__', '__license__']
+__all__ = ["__version__", "__versiondate__", "__license__"]
 
-__version__ = '0.1.0'
-__versiondate__ = '2024-11-12'
-__license__ = 'MIT'
+__version__ = "0.1.0"
+__versiondate__ = "2024-11-12"
+__license__ = "MIT"
