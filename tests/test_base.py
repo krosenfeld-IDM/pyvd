e8aa5f027c8712bef1b955b0478ea99855ac5960
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-17 23:14:57 +0000

    update test

--- tests/test_base.py
+++ tests/test_base.py
@@ -11,14 +11,11 @@ class TestBase(unittest.TestCase):
         year_vec = pop_input[0, :] - pyvd.constants.BASE_YEAR
         year_init = pyvd.constants.BASE_YEAR - pyvd.constants.BASE_YEAR
         pop_mat = pop_input[1:, :] + 0.1
-        pop_init = [np.interp(year_init, year_vec, pop_mat[idx, :])
-                    for idx in range(pop_mat.shape[0])]
-        vd_tup = demog_vd_calc(year_vec, year_init, pop_mat, pop_init)
+        vd_tup = demog_vd_calc(year_vec, year_init, pop_mat)
         
         self.assertEqual(len(vd_tup), 6)
         self.assertIsInstance(vd_tup[0], list)  # mort_year
         self.assertIsInstance(vd_tup[1], np.ndarray)  # mort_mat
-        self.assertIsInstance(vd_tup[2], list)  # age_x
         self.assertIsInstance(vd_tup[3], float)  # birth_rate
         self.assertIsInstance(vd_tup[4], np.ndarray)  # brmultx_02
         self.assertIsInstance(vd_tup[5], np.ndarray)  # brmulty_02
