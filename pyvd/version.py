15804ef0b51a53f0bcd1213a9d273cf473ec5811
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-18 17:13:49 +0000

    Bump version: 1.0.0 → 1.0.1

--- pyvd/version.py
+++ pyvd/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
-__version__ = "1.0.0"
-__versiondate__ = '2025-02-17'
+__version__ = "1.0.1"
+__versiondate__ = '2025-02-18'
 __license__ = "MIT"
